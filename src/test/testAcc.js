003d78b9385b7b6f6cf3689cf21ddf6b0335e17d
cobble586 <cobble2626@gmail.com>
2024-02-05 11:04:55 +0800

    nerve mint kit

---
+++ src/test/testAcc.js
@@ -0,0 +1,29 @@
+const nerve = require("../index");
+require('dotenv').config();
+
+function acc(pri) {
+    let pub = nerve.getPubByPri(pri);
+    let fromAddress = nerve.getAddressByPri(nerve.chainId(), pri);
+    console.log(fromAddress);
+    return {pri, pub, fromAddress};
+}
+
+var testAcc = {
+    acc0() {
+        return acc(process.env.acc0);
+    },
+
+    acc1() {
+        return acc(process.env.acc1);
+    },
+
+    acc2() {
+        return acc(process.env.acc2);
+    },
+
+    acc3() {
+        return acc(process.env.acc3);
+    },
+}
+
+module.exports = testAcc;
